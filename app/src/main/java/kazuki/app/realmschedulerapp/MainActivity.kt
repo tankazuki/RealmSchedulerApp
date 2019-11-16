ba0ac956d9a2458af760f3fbae456f241b244f1f
tankazuki <34229874+tankazuki@users.noreply.github.com>
2019-11-17 02:57:53 +0900

    RecyclerViewの設定を追加

--- app/src/main/java/kazuki/app/realmschedulerapp/MainActivity.kt
+++ app/src/main/java/kazuki/app/realmschedulerapp/MainActivity.kt
@@ -5,15 +5,25 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.recyclerview.widget.LinearLayoutManager
+import io.realm.Realm
+import io.realm.kotlin.where
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var realm: Realm
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+        realm = Realm.getDefaultInstance()
+        list.layoutManager = LinearLayoutManager(this)
+        val schedules = realm.where<Schedule>().findAll()
+        val adapter = ScheduleAdapter(schedules)
+        list.adapter = adapter
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
@@ -35,5 +45,10 @@ class MainActivity : AppCompatActivity() {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        realm.close()
     }
 }
---
+++ app/src/main/java/kazuki/app/realmschedulerapp/ScheduleAdapter.kt
@@ -0,0 +1,41 @@
+package kazuki.app.realmschedulerapp
+
+import android.text.format.DateFormat
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import io.realm.OrderedRealmCollection
+import io.realm.RealmRecyclerViewAdapter
+
+class ScheduleAdapter(data: OrderedRealmCollection<Schedule>): RealmRecyclerViewAdapter<Schedule, ScheduleAdapter.ViewHolder>(data, true) {
+
+    init {
+        setHasStableIds(true)
+    }
+
+    class ViewHolder(cell: View) : RecyclerView.ViewHolder(cell) {
+        val date: TextView = cell.findViewById(android.R.id.text1)
+        val title: TextView = cell.findViewById(android.R.id.text2)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):
+            ScheduleAdapter.ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(android.R.layout.simple_list_item_2, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ScheduleAdapter.ViewHolder, position: Int) {
+        val schedule: Schedule? = getItem(position)
+        holder.date.text = DateFormat.format("yyyy/MM/dd", schedule?.date)
+        holder.date.text = schedule?.title
+    }
+
+    override fun getItemId(position: Int): Long {
+        return getItem(position)?.id ?: 0
+    }
+
+
+}
